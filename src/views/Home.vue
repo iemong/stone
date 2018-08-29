3b30e2a6ade11c87195b5b064c99297b7b5c0df6
irie-shinnosuke <irie-shinnosuke@kayac.com>
2018-08-29 17:03:10 +0900

    add content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <p class="number">{{counter}}秒 / 94608000秒</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import firebase from "firebase";
+const config = {
+  apiKey: "AIzaSyBZqj5mk6EZDtOQHClJGBb3l9Nw0FAqdpA",
+  authDomain: "firabasefire.firebaseapp.com",
+  databaseURL: "https://firabasefire.firebaseio.com",
+  projectId: "firabasefire",
+  storageBucket: "firabasefire.appspot.com",
+  messagingSenderId: "616673046185"
+};
+firebase.initializeApp(config);
+const database = firebase.database();
 
 export default {
   name: "home",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      counter: 0
+    };
+  },
+  methods: {
+    getCounter(val) {
+      this.counter = val;
+    }
+  },
+  mounted() {
+    const starCountRef = firebase.database().ref("stone");
+    starCountRef.on("value", snapshot => {
+      // console.log(snapshot.val());
+      this.getCounter(snapshot.val());
+    });
   }
 };
 </script>
+
+<style lang="scss">
+.home {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+}
+
+.number {
+    font-size: 64px;
+}
+</style>
